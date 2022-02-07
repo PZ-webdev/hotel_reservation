d2c76cd6aa16f3a63819d6036176058198d230b4
Patryk Zaprzała <74723494+PZ-webdev@users.noreply.github.com>
2022-02-07 17:48:28 +0100

    Changing name files

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -7,7 +7,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import org.hibernate.Session;
 
-public class Main extends Application {
+public class App extends Application {
 
     public static void main(String[] args) {
         launch(args);

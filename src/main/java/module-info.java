99d1bd87a0d45ffa4e4df276e917c35407f0e97a
Patryk Zaprzała <74723494+PZ-webdev@users.noreply.github.com>
2022-02-08 09:21:17 +0100

    fix bug with jar file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,20 +1,35 @@
 module com.project {
-    requires javafx.fxml;
+//    requires javafx.fxml;
+//    requires javafx.controls;
+//
+//    requires java.sql;
+//    requires java.persistence;
+//    requires java.naming;
+//
+//    requires org.hibernate.orm.core;
+//    requires lombok;
+//    opens com.project to javafx.fxml;
+//
+//    exports com.project;
+//    exports com.project.Controllers;
+//    exports com.project.Models;
+//    exports com.project.DAO;
+//
+//    opens com.project.Models;
+//    opens com.project.Controllers to javafx.fxml;
     requires javafx.controls;
-
-    requires java.sql;
-    requires java.persistence;
-    requires java.naming;
-
+    requires javafx.fxml;
     requires org.hibernate.orm.core;
     requires lombok;
-    opens com.project to javafx.fxml;
+    requires java.persistence;
+    requires java.naming;
+    requires java.sql;
 
-    exports com.project;
-    exports com.project.Controllers;
     exports com.project.Models;
     exports com.project.DAO;
-
+    exports com.project.Controllers;
     opens com.project.Models;
-    opens com.project.Controllers to javafx.fxml;
+
+    opens com.project to javafx.fxml;
+    exports com.project;
 }
